74c647979b7a3fb184485351e86cc2ab115534d1
aryopanduuu <alianzsky@gmail.com>
2022-11-09 14:27:09 +0700

    route tentang

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,10 @@ Route::get('/', function () {
 	return view('pages.index');
 })->name('home');
 
+Route::get('/tentang', function () {
+	return view('pages.tentang');
+})->name('tentang');
+
 Route::prefix('appointment')->name('appointment.')->group(function () {
 	Route::get('/', 'AppointmentController@index')->name('index');
 	Route::post('/', 'AppointmentController@checkNomorRekamMedis')->name('search');
