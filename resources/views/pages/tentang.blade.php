151885ea0e56e67b8a7f756f5aa464f2419c2d5d
aryopanduuu <alianzsky@gmail.com>
2022-11-09 14:25:46 +0700

    Tentang

--- resources/views/pages/tentang.blade.php
+++ resources/views/pages/tentang.blade.php
@@ -1,3 +1,23 @@
 @extends('layouts.app')
 @section('content')
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <section id="hero" class="d-flex align-items-center">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                    <h1 data-aos="fade-up">Visi & Misi</h1>
+                    <h3 data-aos="fade-up" data-aos-delay="400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut,
+                        delectus error necessitatibus fugiat nam culpa aut illum tempora blanditiis amet illo nulla
+                        repellendus earum quaerat. Ipsa fugit quisquam dolorum voluptatibus.
+                    </h3>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+                    <img src="assets/img/aboutt.png" class="img-fluid animated" alt="">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <script src="assets/vendor/aos/aos.js"></script>
 @endsection
