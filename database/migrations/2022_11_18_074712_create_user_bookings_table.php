5c65e0feda8b2a1eb5b9c70b8a8b9e03ae1f3f10
Rheznendra Praditya <rheznendra@gmail.com>
2022-12-09 22:35:30 +0700

    Fix migrate user booking

--- database/migrations/2022_11_18_074712_create_user_bookings_table.php
+++ database/migrations/2022_11_18_074712_create_user_bookings_table.php
@@ -16,12 +16,14 @@ return new class extends Migration
 		Schema::create('user_bookings', function (Blueprint $table) {
 			$table->uuid('id');
 			$table->char('kode_antrian', 12)->unique();
+			$table->char('snap_token', 36);
 			$table->char('nomor_antrian', 4);
 			$table->uuid('user_id');
 			$table->uuid('dokter_id');
 			$table->uuid('poli_id');
 			$table->date('tgl_periksa');
 			$table->time('perkiraan_jam');
+			$table->tinyInteger('payment_status')->default(0);
 			$table->timestamps();
 			$table->foreign('user_id')->references('id')->on('users');
 			$table->foreign('poli_id')->references('id')->on('polis');
